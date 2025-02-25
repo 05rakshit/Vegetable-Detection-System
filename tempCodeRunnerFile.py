e7f1bd29b6e02f91785eec74e4e37dcea3c50b57
05rakshit <05rakshitgarg@gmail.com>
2025-02-25 19:13:14 +0530

    Modification in exisiting files

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-RGB")
+def convert_to_rgb(image):
+    if isinstance(image, np.ndarray):  
+        image=Image.fromarray(image)
+    if image.mode in ("RGBA", "P"):  
+        image = image.convert("RGB")
+    return np.array(image)
